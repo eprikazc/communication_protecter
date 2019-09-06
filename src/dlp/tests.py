097dad34d8d1fce4e9187781012d5237c913c58c
Eugene Prikazchikov <eprikazc@gmail.com>
2019-09-06 09:30:14 +0300

    Add more tests

--- src/dlp/tests.py
+++ src/dlp/tests.py
@@ -11,13 +11,13 @@ def company_email_rule(db):
 
 
 def test_analyze_ok(company_email_rule):
-    res = analyze('This message is ok')
-    assert res is None
+    task = analyze.s('This message is ok').apply()
+    assert task.result is None
 
 
 def test_analyze_complains(company_email_rule):
-    res = analyze(
+    task = analyze.s(
         'This message is not ok\n'
         'It contains this address: secret@company.com\n'
-        'Therefore DLP should report it')
-    assert res == company_email_rule.id
+        'Therefore DLP should report it').apply()
+    assert task.result == company_email_rule.id
--- src/slack_connector/tests.py
+++ src/slack_connector/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from dlp.models import FilterRule
+from slack_connector.models import DLPDetection
+from slack_connector.tasks import process_slack_message, handle_dlp_result
+
+
+@pytest.fixture
+def company_email_rule(db):
+    return FilterRule.objects.create(
+        name='Company email address', pattern='@company.com')
+
+
+def test_handle_dlp_result_ok(company_email_rule):
+    task = handle_dlp_result.s(None, 'this message is ok', '{...}').apply()
+    assert task.result is None
+
+
+def test_handle_dlp_resul_detection(company_email_rule):
+    task = handle_dlp_result.s(
+        company_email_rule.id, 'this message is ok', '{...}').apply()
+    detections = DLPDetection.objects.all()
+    assert len(detections) == 1
+    assert task.result is detections[0].id
